235cbf1f91505e5a4f63377d585c92dc65d082ff
Prajil <prajilprabhan924@gmail.com>
2026-08-23 22:06:35 +0530

    feat(frontend): set up client-side routes in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+import Navbar from "./components/Navbar";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+function App() {
   return (
-    <div>
-      Hello All
-    </div>
-  )
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+
+      </Routes>
+    <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
